e813040e0d10ae48de428f98c054b7889fc14c0d
Emory <ro25ck@yahoo.com>
2023-10-02 15:31:58 +0100

    removed console logs

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,7 +1,6 @@
 const { fetchTopics } = require("../models/topics.model")
 
 exports.getTopics = (req, res, next) => {
-    console.log("in controller code set")
     fetchTopics().then((topics) => {
         res.status(200).send({ topics });
     }).catch((err) => {
--- models/topics.model.js
+++ models/topics.model.js
@@ -1,7 +1,6 @@
 const db = require('../db/connection');
 
 exports.fetchTopics = () => {
-    console.log("in model code set")
     return db
         .query("SELECT * FROM topics;")
         .then(({rows}) => {
